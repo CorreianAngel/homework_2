d9793fd63d297795f8d9aac2c44440a6751164ac
RomanThinks <61527005+RomanThinks@users.noreply.github.com>
2020-02-27 19:10:22 -0800

    uint start_tick added into the struct proc { }

--- proc.h
+++ proc.h
@@ -50,6 +50,8 @@ struct proc {
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
   int priority;                // process priority
+  
+  uint start_tick;             //additional Homework2 code line
 };
 
 // Process memory is laid out contiguously, low addresses first:
